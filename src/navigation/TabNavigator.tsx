e8a9bd841bc2eef437a8b5bbe0415b6209fece1e
Hafiz <zoujuhasna@gmail.com>
2026-06-28 09:36:36 +0530

    implement Data Integrity PoC and simplify prototype navigation

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import HomeScreen from '../screens/HomeScreen';
-import HistoryScreen from '../screens/HistoryScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import { Colors } from '../constants/Colors';
 
@@ -16,8 +15,6 @@ const TabNavigator = () => {
           let iconName: any;
           if (route.name === 'Home') {
             iconName = focused ? 'flask' : 'flask-outline';
-          } else if (route.name === 'History') {
-            iconName = focused ? 'time' : 'time-outline';
           } else if (route.name === 'Profile') {
             iconName = focused ? 'person' : 'person-outline';
           }
@@ -30,7 +27,6 @@ const TabNavigator = () => {
       })}
     >
       <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Laboratory' }} />
-      <Tab.Screen name="History" component={HistoryScreen} options={{ title: 'Logbook' }} />
       <Tab.Screen name="Profile" component={ProfileScreen} options={{ title: 'Scientist' }} />
     </Tab.Navigator>
   );
